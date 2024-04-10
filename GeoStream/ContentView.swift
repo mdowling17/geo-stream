b958e0937e9fbb8aea76fbf0270d923095e5f754
macM1 <wangzr926@gmail.com>
2024-04-10 17:51:47 -0400

    Initial Commit

---
+++ GeoStream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeoStream
+//
+//  Created by Zirui Wang on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
