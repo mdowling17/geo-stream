5e8a5258540f3f650ca4c52a7a872d7ddc5b0476
Matt Dowling <mdowling17@gmail.com>
2024-04-29 17:09:32 -0400

    Reorganize file structure

--- GeoStream/GeoStreamApp.swift
+++ GeoStream/GeoStreamApp.swift
@@ -6,23 +6,20 @@
 //
 
 import SwiftUI
-import FirebaseCore
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
-}
+import Firebase
 
 @main
 struct GeoStreamApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
+    @StateObject var authVM = AuthViewModel()
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .environmentObject(authVM)
         }
     }
 }
