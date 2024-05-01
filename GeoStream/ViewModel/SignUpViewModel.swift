d0a523ecbb9b1751709149c603464c29fb8e5519
Matt Dowling <mdowling17@gmail.com>
2024-05-01 13:44:08 -0400

    Add AuthService, SignIn, SignUp, SignOut, ForgotPassword, AppColor, AppIcon, CustomTextField, AppHeader

---
+++ GeoStream/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpViewModel.swift
+//  GeoStream
+//
+//  Created by Matthew Dowling on 5/1/24.
+//
+
+import Foundation
+
+class SignUpViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var confirmPassword = ""
+    @Published var showPassword = false
+    @Published var showConfirmPassword = false
+    @Published var errorMessage: String? = nil
+    
+    func signUp() {
+        Task {
+            do {
+                try await AuthService.shared.signUp(email: email, password: password, confirmPassword: password)
+            } catch {
+                print("[DEBUG ERROR] SignUpViewModel:signUp() Error: \(error.localizedDescription)")
+                errorMessage = error.localizedDescription
+            }
+        }
+    }
+}
