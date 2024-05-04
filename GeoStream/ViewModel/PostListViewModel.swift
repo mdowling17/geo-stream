24e367df9305e35c8c92381e4c6f466ad24507cb
macM1 <wangzr926@gmail.com>
2024-05-04 03:50:10 -0400

    added post and user service functions

--- GeoStream/ViewModel/PostListViewModel.swift
+++ GeoStream/ViewModel/PostListViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SwiftUI
+
+@MainActor
+class PostListViewModel: ObservableObject {
+    @Published var posts: [Post] = []
+    @Published var user: User = User(email: "", displayName: "", description: "", photoURL: "", followers: [], following: [], favPost: [])
+    let userId = AuthService.shared.currentUser!.uid
+    
+    func fetchPosts() {
+        Task {
+            let fetchedPosts = await PostService.shared.fetchPostsByUserId(userId)
+            self.posts = fetchedPosts
+        }
+    }
+    
+    func fetchUser() {
+        Task {
+            let fetchedUser = try await UserService.shared.fetchProfile(documentId: userId)
+            self.user = fetchedUser
+        }
+    }
+}
+
+@MainActor
+class PostRowViewModel: ObservableObject {
+    @Published var img: UIImage?
+    @Published var comments: [Comment] = []
+    
+    func fetchUserImg(_ userId: String) {
+        Task {
+            img = try await UserService.shared.fetchProfileImage(documentId: userId)
+        }
+    }
+    
+    func fetchComments(_ postId: String) {
+        print(postId)
+        Task {
+            comments = await PostService.shared.fetchComments(postId)
+            print(comments)
+        }
+    }
+}
