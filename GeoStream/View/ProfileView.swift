c34e197693e48083058bc24e4190225d64e6b2c3
macM1 <wangzr926@gmail.com>
2024-05-04 03:48:50 -0400

    added post and user service functions

---
+++ GeoStream/View/ProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileView.swift
+//  GeoStream
+//
+//  Created by Zirui Wang on 5/3/24.
+//
+
+import Foundation
---
+++ GeoStream/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  GeoStream
+//
+//  Created by Zirui Wang on 5/3/24.
+//
+
+import Foundation
